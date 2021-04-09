59ed021f69f519684aaab1c6524ea6045d5bccf1
Saurav Jain <souravjain540@gmail.com>
2021-04-09 20:15:47 +0530

    Add files via upload

--- Euler4.py
+++ Euler4.py
@@ -1,10 +1,29 @@
 pall_list=[]
 
+def isPallindrome( num ):
+    temp = num
+    rev = 0
+    while(num>0):
+        dig=num%10
+        rev=rev*10+dig
+        num=num//10
+    if(temp==rev):
+        return True
+
 for i in range(100,1000):
     # for iterating through all three digit nums
     for j in range(100,1000):
         # second digit 
-        if(str(i*j)==(str(i*j)[::-1])):
+        if(isPallindrome(i*j)):
             # checking pallindrome
             pall_list.append(i*j)
+
+    
 print(max(pall_list))
+
+
+
+
+
+
+# str(i*j)==(str(i*j)[::-1])
